538fe918d948980231ecd8cc38d0df9b55a7d483
Luis Alvarado <luis.gustavo.2018@outlook.com>
2021-09-17 13:42:23 -0500

    Se agrega capa de datos (manejo de conexion)

---
+++ Data/ApplicationDbContextActivities.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities;
+using System;
+
+namespace Data
+{
+    public class ApplicationDbContextActivities: DbContext
+    {
+        public ApplicationDbContextActivities(DbContextOptions<ApplicationDbContextActivities> options): base(options)
+        {
+
+        }
+
+        public DbSet<Property> Property { get; set; }
+    }
+    
+}
