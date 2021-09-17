eac344b713a752bd6e1e672b8bdc21c52347f86a
Luis Alvarado <luis.gustavo.2018@outlook.com>
2021-09-17 13:08:16 -0500

    Agregar archivos de proyecto.

---
+++ Entities/Property.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class Property
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Descripcion { get; set; }
+
+        [Required]
+        public DateTime Created_At { get; set; }
+
+        [Required]
+        public DateTime Update_At { get; set; }
+        
+        public DateTime? Disabled_At { get; set; }
+
+        [Required]
+        [MaxLength(35)]
+        public string Status { get; set; }
+    }
+}
